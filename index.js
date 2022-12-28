264b19e64780a28827fa323c60575ff37a704777
Swapnil Sable <32626980+sable5499@users.noreply.github.com>
2022-12-28 20:33:21 +0530

    Last

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
-function getResume(){
-  window.open("https://drive.google.com/file/d/1HO24zhMhHHEMyYIS64apeAZ3pJBlizof/view?usp=sharing");
+const form = document.querySelector("#form");
+
+const people = [];
+
+function addUsers() {
+
+  let f_name = document.getElementById("fname").value;
+  let l_name = document.getElementById("lname").value;
+  let email_id = document.getElementById("emailId").value;
+  let contact = document.getElementById("contactNum").value;
+  let msg = document.getElementById("message").value;
+
+  people.push({
+    First_Name: f_name,
+    Last_Name: l_name,
+    Email: email_id,
+    Contact: contact,
+    Message: msg,
+  });
+
+  form.reset();
 }
 
-function goHome(){
-  window.open.href = "/index.html";
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("send-btn");
+// var span = document.getElementsByClassName("close")[0];
+//
+// // Open modal
+//
+// btn.onclick = function(){
+//   modal.style.display = "block";
+// }
+//
+// // Close modal
+//
+// btn.onclick = function(){
+//   modal.style.display = "none";
+// }
+//
+// // Anywhere outside box
+//
+// window.onclick = function(event){
+//   if (event.target == modal){
+//     modal.style.display = "none";
+//   }
+// }
+
+
+function getResume() {
+  window.open("https://drive.google.com/file/d/1HO24zhMhHHEMyYIS64apeAZ3pJBlizof/view?usp=sharing");
 }
